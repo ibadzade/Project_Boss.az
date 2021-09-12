9956bdcc2e7de54f1d6b2eb2008f0742e65603d2
Ibad_3qug <Ibad_3qug@itstep.edu.az>
2021-09-13 01:52:58 +0300

    Добавьте файлы проекта.

---
+++ Project_Boss.az/Class/Cv.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectFinal.Classes
+{
+    public class Cv
+    {
+        public int Id { get; set; }
+        public string Speciality { get; set; }
+        public string FirstSchool { get; set; }
+        public int Score { get; set; }
+        public List<Skill> Skills { get; set; }
+        public List<Companie> Companies { get; set; }
+        public List<Language> Languages { get; set; }
+        public bool IsDiplom { get; set; }
+
+        public override string ToString()
+        {
+            return $@"ID : {Id}
+            Speciality : {Speciality}
+            School : {FirstSchool}
+            Score : {Score}
+            Diplom : {IsDiplom}
+";
+        }
+        public void Show()
+        {
+            Console.WriteLine("==========Cv Info=======");
+            Console.WriteLine($"ID : {Id}");
+            Console.WriteLine($"Speciality : {Speciality}");
+            Console.WriteLine($"School : {FirstSchool}");
+            Console.WriteLine($"Score : {Score}");
+            Console.WriteLine($"Diplom : {IsDiplom}");
+
+            foreach (var item in Skills)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            foreach (var item in Companies)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            foreach (var item in Languages)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
---
+++ Project_Boss.az/Exception/TryParse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectFinal.Exception1
+{
+    class TryParse : ApplicationException
+    {
+        public TryParse(string message) : base(message)
+        {
+
+        }
+    }
+}
